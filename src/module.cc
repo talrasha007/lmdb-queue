211143dc99f311fa0eedfe9494ad143ef243a198
Tal Rasha <talrasha007@gmail.com>
2015-05-15 16:22:55 +0800

    consumer

--- src/module.cc
+++ src/module.cc
@@ -1,6 +1,7 @@
 #include <nan.h>
 
 #include "producer.h"
+#include "consumer.h"
 
 using namespace std;
 using namespace v8;
@@ -114,11 +115,99 @@ private:
     Producer _handle;
 };
 
+template<DataType DT> class ReturnMaker {
+public:
+    Local<Value> static make(const Consumer::ItemType& item);
+};
+
+template<> Local<Value> ReturnMaker<STRING_TYPE>::make(const Consumer::ItemType& item) {
+    return NanNew(std::get<1>(item));
+}
+
+template<> Local<Value> ReturnMaker<BUFFER_TYPE>::make(const Consumer::ItemType& item) {
+    return NanNewBufferHandle(std::get<1>(item), std::get<2>(item));
+}
+
+class ConsumerWrap : public ObjectWrap {
+public:
+    static void setup(Handle<Object>& exports) {
+        const char* className = "Consumer";
+
+        Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(ConsumerWrap::ctor);
+        tpl->SetClassName(NanNew(className));
+        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+        NODE_SET_PROTOTYPE_METHOD(tpl, "pullString", ConsumerWrap::pull<STRING_TYPE>);
+        NODE_SET_PROTOTYPE_METHOD(tpl, "pullBuffer", ConsumerWrap::pull<BUFFER_TYPE>);
+
+        exports->Set(NanNew(className), tpl->GetFunction());
+    }
+
+private:
+    static NAN_METHOD(ctor) {
+        NanScope();
+
+        Handle<Object> opt = args[0]->ToObject();
+
+        NanUtf8String path(opt->Get(NanNew("path")));
+        NanUtf8String topicName(opt->Get(NanNew("topic")));
+        NanUtf8String name(opt->Get(NanNew("name")));
+
+        TopicOpt topicOpt{ 1024 * 1024 * 1024, 8 };
+        Local<Value> chunkSize = opt->Get(NanNew("chunkSize"));
+        Local<Value> chunksToKeep = opt->Get(NanNew("chunksToKeep"));
+        if (chunkSize->IsNumber()) topicOpt.chunkSize = size_t(chunkSize->NumberValue());
+
+        ConsumerWrap* ptr = new ConsumerWrap(*path, *topicName, *name, &topicOpt);
+        ptr->Wrap(args.This());
+        NanReturnValue(args.This());
+    }
+
+    static NAN_METHOD(offset) {
+        NanScope();
+
+        ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(args.This());
+
+        if (ptr->_cur <= ptr->_batch.size()) {
+            NanReturnValue(NanNew(double(std::get<0>(ptr->_batch.at(ptr->_cur - 1)))));
+        }
+
+        NanReturnUndefined();
+    }
+
+    template<DataType DT> static NAN_METHOD(pull) {
+        NanScope();
+
+        ConsumerWrap* ptr = ObjectWrap::Unwrap<ConsumerWrap>(args.This());
+
+        if (ptr->_cur < ptr->_batch.size()) {
+            NanReturnValue(ReturnMaker<DT>::make(ptr->_batch.at(ptr->_cur++)));
+        }
+
+        ptr->_batch.clear();
+        ptr->_cur = 1;
+        ptr->_handle.pull(ptr->_batch, 128);
+        if (ptr->_batch.size() > 0) {
+            NanReturnValue(ReturnMaker<DT>::make(ptr->_batch.at(0)));
+        }
+
+        NanReturnUndefined();
+    }
+
+private:
+    ConsumerWrap(const char* path, const char* topicName, const char* name, TopicOpt* opt) : _handle(path, topicName, name, opt), _cur(0) { }
+
+    Consumer _handle;
+    Consumer::BatchType _batch;
+    int _cur;
+};
+
 void init(v8::Handle<v8::Object> exports) {
     exports->Set(NanNew("STRING_TYPE"), NanNew(STRING_TYPE));
     exports->Set(NanNew("BUFFER_TYPE"), NanNew(BUFFER_TYPE));
 
     ProducerWrap::setup(exports);
+    ConsumerWrap::setup(exports);
 }
 
 NODE_MODULE(lmdb_queue, init);
